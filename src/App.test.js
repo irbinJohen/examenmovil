fe07ffe556b4fac67025b9eedd2790cecb4f3a86
irbinJohen <pipen_yoen@gmail.com>
2023-09-22 20:07:28 -0700

    examen

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import Form from './comp.js';
+import '.App.css'
+
+function App(){
+  return(
+    <div
+    className='App'>
+
+    <Form> </Form>
+
+  </div>
+  )
+}
+
